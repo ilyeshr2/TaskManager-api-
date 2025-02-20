fc07f14b5f45381094ffb7362752e4d46ad251d0
ilies harrache <128542440+ilyeshr2@users.noreply.github.com>
2025-02-20 17:37:08 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
 import Vue from 'nativescript-vue';
 import Login from './components/Login.vue';
 
-
-// Add this at the start of app.js
 Vue.config.silent = false;
 new Vue({
     render: h => h('frame', [h(Login)])
